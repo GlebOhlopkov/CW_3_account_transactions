29ad7c7be3b8f1a621d08c2bf803a81bdeaab77c
glebokhlopkov <glebka_90@mail.ru>
2023-08-13 19:37:01 +0300

    add files utils.py and tests for it renamed tests dir

---
+++ main/utils.py
@@ -0,0 +1,45 @@
+import json
+from datetime import datetime
+
+
+def create_transactions_list(filename: str) -> list:
+    """
+    Метод для экспорта списка транзакций из файла
+    :param filename: наименования файла для чтения
+    :return: список транзакций
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        transactions_list = json.load(file)
+    return transactions_list
+
+
+def format_date(date_iso: str) -> str:
+    """
+    Метод для перевода даты в формат ДД.ММ.ГГГГ
+    :param date_iso: дата в формате ISO 8601
+    :return: дата в формате ДД.ММ.ГГГГ
+    """
+    transaction_date = datetime.strptime(date_iso, '%Y-%m-%dT%H:%M:%S.%f')
+    return transaction_date.strftime('%d.%m.%Y')
+
+
+def number_mask(number: str) -> str:
+    """
+    Метод для наложения маски на метод оплаты/приема транзакций
+    :param number: номер счета/банковской карты
+    :return: номер счета/банковской карты с шифрованным числовым значением
+    """
+    list_number = number.split(' ')
+    if list_number[0] == "Счет":
+        result_number = ' '.join(list_number)
+        return f'{list_number[0]} **{result_number[-4:]}'
+    if list_number[0] == "Maestro":
+        result_number = ' '.join(list_number)
+        return f'{list_number[0]} {result_number[-16:-12]} {result_number[-12:-10]}** **** {result_number[-4:]}'
+    if list_number[0] == "MasterCard":
+        result_number = ' '.join(list_number)
+        return f'{list_number[0]} {result_number[-16:-12]} {result_number[-12:-10]}** **** {result_number[-4:]}'
+    if list_number[0] == "Visa":
+        result_number = ' '.join(list_number)
+        return (f'{list_number[0]} {list_number[1]} {result_number[-16:-12]} '
+                f'{result_number[-12:-10]}** **** {result_number[-4:]}')
